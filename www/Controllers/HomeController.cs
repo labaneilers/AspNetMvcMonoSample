6f2052f0e992cc3f8383b2ae39393f4d0ed8b856
Laban Eilers <laban@eilers.com>
2015-01-03 14:58:42 -0500

    Initial commit

---
+++ www/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+namespace www.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName ();
+			var isMono = Type.GetType ("Mono.Runtime") != null;
+
+			ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+			ViewData["Runtime"] += " Framework version: " + Environment.Version + " " + Environment.OSVersion + " 64bit: " + Environment.Is64BitProcess;
+
+			var asms = new List<string> ();
+
+			foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
+				asms.Add(DumpAssembly(asm));
+			}
+
+			asms.Sort();
+
+			ViewData["Assemblies"] = asms;
+
+			return View ();
+		}
+
+		private static string DumpAssembly(System.Reflection.Assembly asm) {
+			return asm.FullName;
+		}
+	}
+}
+
